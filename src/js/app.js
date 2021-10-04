9d184c407bef96bbc8dd17a297f4803f4d6b0d83
acrysense <54480354+acrysense@users.noreply.github.com>
2021-10-04 22:40:26 +0300

    add: Added new pages and improvements

--- src/js/app.js
+++ src/js/app.js
@@ -13,6 +13,74 @@ document.addEventListener('DOMContentLoaded', function () {
         document.documentElement.style.setProperty('--height', `${header.getBoundingClientRect().height}px`);
     });
 
+    // SMOOTH SCROLL
+    function currentYPosition() {
+        // Firefox, Chrome, Opera, Safari
+        if (self.pageYOffset) return self.pageYOffset;
+
+        // Internet Explorer 6 - standards mode
+        if (document.documentElement && document.documentElement.scrollTop)
+            return document.documentElement.scrollTop;
+        
+        // Internet Explorer 6, 7 and 8
+        if (document.body.scrollTop) return document.body.scrollTop;
+
+        return 0;
+    } 
+    
+    function elmYPosition(eID) {
+        let elm = document.getElementById(eID);
+        let y = elm.offsetTop;
+        let node = elm;
+        while (node.offsetParent && node.offsetParent != document.body) {
+            node = node.offsetParent;
+            y += node.offsetTop;
+        } return y;
+    }
+    
+    function smoothScroll(eID) {
+        let startY = currentYPosition();
+        let stopY = elmYPosition(eID);
+        let anchorsPosition = document.querySelector('.anchors');
+        if (anchorsPosition) {
+            stopY = elmYPosition(eID) - Number(anchorsPosition.getBoundingClientRect().height);
+        }
+        let distance = stopY > startY ? stopY - startY : startY - stopY;
+        if (distance < 100) {
+            scrollTo(0, stopY); return;
+        }
+        let speed = Math.round(distance / 100);
+        if (speed >= 20) speed = 20;
+        let step = Math.round(distance / 25);
+        let leapY = stopY > startY ? startY + step : startY - step;
+        let timer = 0;
+        if (stopY > startY) {
+            for (let i = startY; i < stopY; i += step ) {
+                setTimeout('window.scrollTo(0, ' + leapY + ')', timer * speed);
+                leapY += step; if (leapY > stopY) leapY = stopY; timer++;
+            } return;
+        }
+        for (let i = startY; i > stopY; i -= step ) {
+            setTimeout('window.scrollTo(0, ' + leapY + ')', timer * speed);
+            leapY -= step; if (leapY < stopY) leapY = stopY; timer++;
+        }
+    }
+
+    // ALL LINKS SMOOTH SCROLL
+    const allLinks = document.querySelectorAll('a[href^="#"]')
+
+    if (allLinks) {
+        allLinks.forEach(item => {
+            item.addEventListener('click', (event) => {
+                event.preventDefault()
+        
+                if (item.getAttribute('href').length > 1) {
+                    smoothScroll(item.getAttribute('href').slice(1))
+                }
+            })
+        })
+    }
+
     // FOOTER SMOOTH SCROLL TOP
     const footerUpBtn = document.querySelector('.footer__up')
 
